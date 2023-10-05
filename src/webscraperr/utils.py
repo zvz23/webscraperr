681bc8cee7798a8eb39bdbaed542124c396ab46f
zvz23 <ziegfredzorrilla23@gmail.com>
2023-10-05 18:24:28 +0800

    added utils

--- src/webscraperr/utils.py
+++ src/webscraperr/utils.py
@@ -1,3 +1,10 @@
+from .db import get_db_class_by_config
 def load_urls(urls_file: str):
     with open(urls_file, 'r') as f:
         return f.read().split('\n')
+    
+
+def save_urls(database_config: dict, urls: list):
+    to_save_urls = [[i] for i in urls]
+    with get_db_class_by_config(database_config)(database_config) as conn:
+        conn.save_urls(to_save_urls)
