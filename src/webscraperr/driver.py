c938e31dc4beb5b20cc1231d21e60d9fa0f6acab
zvz23 <ziegfredzorrilla23@gmail.com>
2023-11-14 06:45:18 +0800

    removed profile setting in driver

--- src/webscraperr/driver.py
+++ src/webscraperr/driver.py
@@ -3,11 +3,12 @@ import sys
 import seleniumwire.undetected_chromedriver as uc
     
 def get_driver(config: dict):
-    profile_path = os.path.join(os.path.expanduser('~'), 'Chrome Profiles', config['PROFILE_NAME'])
     driver = None
     if config['OPTIONS']:
-        driver = uc.Chrome(options=config['OPTIONS'], user_data_dir=profile_path)
+        driver = uc.Chrome(options=config['OPTIONS'])
     else:
-        driver = uc.Chrome(user_data_dir=profile_path)
+        options = uc.ChromeOptions()
+        options.headless = False
+        driver = uc.Chrome(options=options)
     driver.maximize_window()
     return driver
